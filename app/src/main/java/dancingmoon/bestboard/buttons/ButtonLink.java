053893911e1e0b4129241b2bb9b94c28cfade0a8
palmodroid <palmodroid@gmail.com>
2016-01-29 22:39:34 +0100

    Work. Temp.

--- app/src/main/java/dancingmoon/bestboard/buttons/ButtonLink.java
+++ app/src/main/java/dancingmoon/bestboard/buttons/ButtonLink.java
@@ -3,22 +3,23 @@ package dancingmoon.bestboard.buttons;
 import android.graphics.Canvas;
 
 import dancingmoon.bestboard.debug.Debug;
+import dancingmoon.bestboard.parser.Tokenizer;
 import dancingmoon.bestboard.scribe.Scribe;
 import dancingmoon.bestboard.states.BoardLinks;
 
 public class ButtonLink extends ButtonMultiTouch implements
         Button.ChangingButton, Cloneable
     {
-    private int index;
+    private Long id;
     private boolean lockKey;
 
     // USE table is filled up only after the definition of the boards
     // At the time of the definition of the USE keys, no data is
     // available about the table, so index verification is not possible
-    // invalid index means: GO BACK
-    public ButtonLink( int index, boolean lockKey )
+    // null id means: GO BACK
+    public ButtonLink( Long id, boolean lockKey )
         {
-        this.index = index;
+        this.id = id;
         this.lockKey = lockKey;
         }
 
@@ -28,24 +29,25 @@ public class ButtonLink extends ButtonMultiTouch implements
         return (ButtonLink)super.clone();
         }
 
-    // MAX_LINKS is only one hexadecimal digit !!
     @Override
     public String getString()
         {
-        if ( index < 0 || index >= BoardLinks.MAX_LINKS )
+        if ( id == null )
             return "PREV";
         else
-            return (lockKey ? "L" : "") + Integer.toHexString( index );
+            return (lockKey ? "L" : "") + Tokenizer.regenerateKeyword(id);
         }
 
     @Override
     public void drawChangingButton(Canvas canvas)
         {
-        if ( layout.softBoardData.boardLinks.getState( index ) == BoardLinks.ACTIVE )
+        int state = layout.softBoardData.boardLinks.getState( id );
+
+        if ( state == BoardLinks.ACTIVE )
             drawButton( canvas, layout.softBoardData.metaColor, layout.layoutXOffset, layout.layoutYOffset);
-        else if ( layout.softBoardData.boardLinks.getState( index ) == BoardLinks.LOCKED )
+        else if ( state == BoardLinks.LOCKED )
             drawButton( canvas, layout.softBoardData.lockColor, layout.layoutXOffset, layout.layoutYOffset);
-        else if ( layout.softBoardData.boardLinks.getState( index ) == BoardLinks.TOUCHED &&
+        else if ( state == BoardLinks.TOUCHED &&
                 layout.softBoardData.displayTouch)
             drawButton( canvas, layout.softBoardData.touchColor, layout.layoutXOffset, layout.layoutYOffset);
         // If state == HIDDEN, then no redraw is needed
@@ -58,18 +60,18 @@ public class ButtonLink extends ButtonMultiTouch implements
 
         if ( phase == ButtonMultiTouch.META_TOUCH )
             {
-            Scribe.debug( Debug.BUTTON, "Index " + index + " USE Button TOUCH.");
-            layout.softBoardData.boardLinks.touch( index );
+            Scribe.debug( Debug.BUTTON, "Board " + Tokenizer.regenerateKeyword(id) + " LINK Button TOUCH.");
+            layout.softBoardData.boardLinks.touch( id );
             }
         else if ( phase == ButtonMultiTouch.META_RELEASE )
             {
-            Scribe.debug( Debug.BUTTON, "Index " + index + " USE Button RELEASE.");
-            layout.softBoardData.boardLinks.release( index, lockKey );
+            Scribe.debug( Debug.BUTTON, "Board " + Tokenizer.regenerateKeyword(id) + " LINK Button RELEASE.");
+            layout.softBoardData.boardLinks.release( id, lockKey );
             }
         else if ( phase == ButtonMultiTouch.META_CANCEL )
             {
-            Scribe.debug( Debug.BUTTON, "Index " + index + " USE Button CANCEL.");
-            layout.softBoardData.boardLinks.cancel( index );
+            Scribe.debug( Debug.BUTTON, "Board " + Tokenizer.regenerateKeyword(id) + " LINK Button CANCEL.");
+            layout.softBoardData.boardLinks.cancel( id );
             }
         }
     }
