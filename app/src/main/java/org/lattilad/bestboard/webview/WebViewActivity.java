502e172ad39c5bd23a1eff05750fe872215e4c67
palmodroid <palmodroid@gmail.com>
2016-05-06 08:36:51 +0200

    Home. Version ready.

--- app/src/main/java/org/lattilad/bestboard/webview/WebViewActivity.java
+++ app/src/main/java/org/lattilad/bestboard/webview/WebViewActivity.java
@@ -2,6 +2,7 @@ package org.lattilad.bestboard.webview;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,10 +11,12 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -25,9 +28,23 @@ import java.io.File;
 /**
  * Simple webview to show html text
  * https://developer.chrome.com/multidevice/webview/gettingstarted
+ *
+ * WebView should be retained between activity starts BUT!
+ * WebView contains the context, so simple fragment retention will leak.
+ * Activity should be retained.
+ * http://www.devahead.com/blog/2012/01/preserving-the-state-of-an-android-webview-on-screen-orientation-change/
  */
 public class WebViewActivity extends Activity
     {
+    // Layout elements
+    private WebView webView;
+    private FrameLayout webViewFrame;
+
+    private ProgressBar progressBar;
+    private RelativeLayout toolLayout;
+    private EditText filter;
+
+
     // Extra of Intents
     static final public String ASSET = "ASSET";
     static final public String FILE = "FILE";
@@ -35,22 +52,90 @@ public class WebViewActivity extends Activity
 
     static final public String SEARCH = "SEARCH";
 
-    private WebView webView;
-    private ProgressBar progressBar;
-    private RelativeLayout toolLayout;
-    private EditText filter;
-
-
-    public void onCreate(Bundle savedInstanceState)
+    protected void initContentView( )
         {
-        super.onCreate(savedInstanceState);
-
         setContentView(R.layout.web_view_activity);
 
-        webView = (WebView) findViewById(R.id.webView);
         toolLayout = (RelativeLayout) findViewById(R.id.tool);
         progressBar = (ProgressBar) findViewById(R.id.progress); // default max value = 100
         filter = (EditText) findViewById(R.id.filter);
+
+        // Retrieve WebView placeholder
+        webViewFrame = ((FrameLayout)findViewById(R.id.webViewFrame));
+
+        // Initialize the WebView at new starts
+        if (webView == null)
+            {
+            // Create the webview
+            webView = new WebView(this);
+            webView.setLayoutParams(
+                    new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            webView.getSettings().setSupportZoom(true);
+            webView.getSettings().setBuiltInZoomControls(true);
+            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.setScrollbarFadingEnabled(true);
+            webView.getSettings().setLoadsImagesAutomatically(true);
+
+            // Load the URLs inside the WebView, not in the external web browser
+            webView.setWebViewClient(new WebViewClient());
+
+            // Load a page
+            String string = null;
+            Uri uri;
+
+            // extra: ASSET
+            if ( ( string = getIntent().getStringExtra(ASSET) ) != null )
+                {
+                string = "file:///android_asset/" + string;
+                }
+
+            // extra: WORK
+            else if ( ( string = getIntent().getStringExtra(WORK) ) != null &&
+                    Environment.getExternalStorageState().equals( Environment.MEDIA_MOUNTED ))
+                {
+                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String directoryName =
+                        sharedPrefs.getString( getString( R.string.descriptor_directory_key ),
+                                getString( R.string.descriptor_directory_default ));
+                File directoryFile = new File( Environment.getExternalStorageDirectory(), directoryName );
+
+                string = Uri.fromFile( new File( directoryFile, string ) ).toString();
+                }
+
+            // extra: FILE
+            else if ( ( string = getIntent().getStringExtra(FILE) ) != null &&
+                    Environment.getExternalStorageState().equals( Environment.MEDIA_MOUNTED ))
+                {
+                string = Uri.fromFile( new File( Environment.getExternalStorageDirectory(), string ) ).toString();
+                }
+
+            // data: uri
+            else if ( ( uri = getIntent().getData() ) != null )
+                {
+                string = uri.toString();
+                }
+
+            if ( string != null )
+                {
+                webView.loadUrl(string);
+                }
+            else
+                {
+                String customHtml = "<html><body>No uri can be found!</body></html>";
+                webView.loadData(customHtml, "text/html", "UTF-8");
+                }
+
+            // extra: SEARCH
+            if ( ( string = getIntent().getStringExtra(SEARCH) ) != null )
+                filter.setText( string );
+            }
+
+        String searchText = filter.getText().toString();
+        toolLayout.setVisibility(searchText.equals("") ? View.GONE : View.VISIBLE );
+
+        // Attach the WebView to its placeholder
+        webViewFrame.addView(webView);
 
         findViewById(R.id.back).setOnClickListener(
                 new View.OnClickListener()
@@ -61,19 +146,16 @@ public class WebViewActivity extends Activity
                     webView.findNext(false); // goBack();
                     }
                 });
+
         findViewById(R.id.forth).setOnClickListener(
                 new View.OnClickListener()
                 {
                 @Override
                 public void onClick(View v)
                     {
-                    webView.findNext( true ); // goForward();
+                    webView.findNext(true); // goForward();
                     }
                 });
-
-        // It is needed only for real web-pages
-        // webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setBuiltInZoomControls(true);
 
         webView.setWebChromeClient(
                 new WebChromeClient()
@@ -86,89 +168,72 @@ public class WebViewActivity extends Activity
 
         webView.setWebViewClient(
                 new WebViewClient()
+                {
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+                    {
+                    Toast.makeText(WebViewActivity.this, description, Toast.LENGTH_SHORT).show();
+                    }
+
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon)
+                    {
+                    progressBar.setVisibility(View.VISIBLE);
+                    }
+
+                // http://stackoverflow.com/a/32720167
+                @Override
+                public void onPageFinished(WebView view, String url)
+                    {
+                    progressBar.setVisibility(View.GONE);
+
+                    String searchText = filter.getText().toString();
+                    if (!searchText.equals(""))
                         {
-                        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
-                            {
-                            Toast.makeText(WebViewActivity.this, description, Toast.LENGTH_SHORT).show();
-                            }
-
-                        @Override
-                        public void onPageStarted(WebView view, String url, Bitmap favicon)
-                            {
-                            progressBar.setVisibility(View.VISIBLE);
-                            }
-
-                        // http://stackoverflow.com/a/32720167
-                        @Override
-                        public void onPageFinished(WebView view, String url)
-                            {
-                            progressBar.setVisibility( View.GONE );
-
-                            String searchText = filter.getText().toString();
-                            if ( !searchText.equals("") )
-                                {
-                                webView.findAllAsync(searchText);
-                                }
-                            }
-                        });
-
-        String string = null;
-        Uri uri;
-
-        // extra: ASSET
-        if ( ( string = getIntent().getStringExtra(ASSET) ) != null )
-            {
-            string = "file:///android_asset/" + string;
-            }
-
-        // extra: WORK
-        else if ( ( string = getIntent().getStringExtra(WORK) ) != null &&
-                Environment.getExternalStorageState().equals( Environment.MEDIA_MOUNTED ))
-            {
-            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-            String directoryName =
-                    sharedPrefs.getString( getString( R.string.descriptor_directory_key ),
-                            getString( R.string.descriptor_directory_default ));
-            File directoryFile = new File( Environment.getExternalStorageDirectory(), directoryName );
-
-            string = Uri.fromFile( new File( directoryFile, string ) ).toString();
-            }
-
-        // extra: FILE
-        else if ( ( string = getIntent().getStringExtra(FILE) ) != null &&
-                Environment.getExternalStorageState().equals( Environment.MEDIA_MOUNTED ))
-            {
-            string = Uri.fromFile( new File( Environment.getExternalStorageDirectory(), string ) ).toString();
-            }
-
-        // data: uri
-        else if ( ( uri = getIntent().getData() ) != null )
-            {
-            string = uri.toString();
-            }
-
-        if ( string != null )
-            {
-            webView.loadUrl( string );
-            }
-        else
-            {
-            String customHtml = "<html><body>No uri can be found!</body></html>";
-            webView.loadData( customHtml, "text/html", "UTF-8");
-            }
-
-        // extra: SEARCH
-        if ( ( string = getIntent().getStringExtra(SEARCH) ) != null )
-            {
-            filter.setText( string );
-            toolLayout.setVisibility( View.VISIBLE );
-            }
-        else
-            {
-            toolLayout.setVisibility( View.GONE );
-            }
+                        webView.findAllAsync(searchText);
+                        }
+                    }
+                });
 
         }
+
+
+    protected void onCreate(Bundle savedInstanceState)
+        {
+        super.onCreate(savedInstanceState);
+        initContentView();
+        }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+        {
+        if (webView != null)    webViewFrame.removeView(webView);
+
+        super.onConfigurationChanged(newConfig);
+
+        initContentView();
+        }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+        {
+        super.onSaveInstanceState(outState);
+
+        // Save the state of the WebView
+        webView.saveState(outState);
+        }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+        {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore the state of the WebView
+        webView.restoreState(savedInstanceState);
+        }
+
 
     /**
      * BACK functions like BACK on the toolbar, but at the last step this BACK can exit
@@ -186,6 +251,7 @@ public class WebViewActivity extends Activity
             }
         }
 
+
 /*  This method is not necessary, onBackPressed is better
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
@@ -202,6 +268,7 @@ public class WebViewActivity extends Activity
         }
 */
 
+
     // options menu contains only a dummy menu entry
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -210,6 +277,7 @@ public class WebViewActivity extends Activity
         getMenuInflater().inflate(R.menu.web_view_menu, menu);
         return true;
         }
+
 
     // option menu is be never displayed,
     // but menu button toggles toolbar visibility
@@ -222,6 +290,7 @@ public class WebViewActivity extends Activity
                 View.GONE : View.VISIBLE );
         return false;
         }
+
 
     // This is not needed, because there is only a dummy entry
     @Override
@@ -242,5 +311,4 @@ public class WebViewActivity extends Activity
 */
         return super.onOptionsItemSelected(item);
         }
-
     }
